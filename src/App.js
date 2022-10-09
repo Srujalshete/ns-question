18c4309c18439daa60aaa1a9aa9d848d03740adf
Srujal Shete <67551839+Srujalshete@users.noreply.github.com>
2022-10-09 16:00:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [id, setId] = useState(1);
+  //code here
+  useEffect(() => {
+    fetch(`https://content.newtonschool.co/v1/pr/main/users/${id}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data.name);
+        setName(data.name);
+      });
+  }, [id]);
+  const changeInput = (e) => {
+    setId(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1 id="text">Type a number between 1 and 10</h1>
+      <input id="input" onChange={(e) => changeInput(e)} />
+      <p id="name">{name}</p>
+    </div>
+  );
+};
+
+export default App;
